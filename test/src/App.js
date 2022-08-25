3b6ec50c5a60e4d9018a9e6266f1a7802c0d95e9
Andrew Nguyen <andrewdlqnguyen@gmail.com>
2022-08-24 20:12:38 -0400

    global and dashboard css

--- test/src/App.js
+++ test/src/App.js
@@ -1,18 +1,17 @@
 import Dashboard from "./pages/Dashboard/Dashboard";
 import CampaignProvider from "./store/CampaignProvider";
+import "./App.css";
 
 /**
  * App will be lean and contain only components, wrappers, routes.
  * @returns Dashboard of the Campaign Case
  */
 const App = () => {
-  return (
-    <CampaignProvider>
-      <Dashboard />
-    </CampaignProvider>
-  );
-}
+    return (
+        <CampaignProvider>
+            <Dashboard className="global-styles"/>
+        </CampaignProvider>
+    );
+};
 
 export default App;
-
-
--- test/src/pages/Dashboard/Dashboard.jsx
+++ test/src/pages/Dashboard/Dashboard.jsx
@@ -1,43 +1,40 @@
 import { useState, useEffect } from "react";
+import { Container, Row } from "react-bootstrap";
 import CampaignList from "../../components/CampaignList/CampaignList";
-import ItemPool from "../../components/ItemPool/ItemPool";
 import SearchBar from "../../components/SearchBar/SearchBar";
 import { Fetch } from "../../helper/helper";
+import * as containerStyles from "./Dashboard.module.css";
 
 const Dashboard = () => {
-  const [campaignList, setCampaignList] = useState();
-  const [selectedCampaign, setSelectedCampaign] = useState();
+    const [campaignList, setCampaignList] = useState();
 
-  useEffect(() => {
-    Fetch("http://localhost:4000/campaigns", (data) => {
-      setCampaignList(data.campaigns);
-    });
-  }, []);
+    useEffect(() => {
+        Fetch("http://localhost:4000/campaigns", (data) => {
+            setCampaignList(data.campaigns);
+        });
+    }, []);
 
-  const selectedCampaignHandler = (selectedCampaign) => {
-
-  }
-
-  let campaignItems = "";
-
-  // if (campaignList !== undefined) {
-  //   campaignItems = campaignList.campaigns.map((campaign) => 
-    
-  //     <div>{campaign.name}</div>
-  //   );
-  // }
-
-  return (
-    <>
-      {campaignList && <SearchBar campaignList={campaignList}/>}
-      {/* {campaignList && <SearchBar campaignList={campaignList} setSelectedCampaign={setSelectedCampaign}/>} */}
-
-      {selectedCampaign && <div>{JSON.stringify(selectedCampaign)}</div>}
-      {/* {!campaignList ? <></> : <div>{campaignList}</div>} */}
-      {/* <ItemPool selectedCampaignItem={selectedCampaign}/> */}
-      <CampaignList />
-    </>
-  );
+    return (
+        <>
+            <Container>
+                <Row className="mx-5">
+                    <div className={` ${containerStyles.title} mb-5`}>
+                        <div className={`${containerStyles.header} global-boldStyles`}>
+                            <span className={containerStyles.companyName}>
+                                Loblaw{" "}
+                            </span>
+                            <span className={containerStyles.companySubName}>
+                                Technology
+                            </span>
+                        </div>
+                        <div className={`${containerStyles.subHeader} global-mediumStyles`}>Performance Metric System</div>
+                    </div>
+                    {campaignList && <SearchBar campaignList={campaignList} />}
+                    <CampaignList />
+                </Row>
+            </Container>
+        </>
+    );
 };
 
 export default Dashboard;
